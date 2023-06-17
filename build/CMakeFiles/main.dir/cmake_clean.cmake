696362f70a0adaab1f55267b45fd7c2119df864c
langningc2009 <jpxx_chenlangning@outlook.com>
2023-06-17 17:21:44 +0800

    first commit

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/home/langningc2009/Coding/Lib/Curl.cpp.o"
+  "CMakeFiles/main.dir/home/langningc2009/Coding/Lib/Curl.cpp.o.d"
+  "CMakeFiles/main.dir/home/langningc2009/Coding/Lib/Exception.cpp.o"
+  "CMakeFiles/main.dir/home/langningc2009/Coding/Lib/Exception.cpp.o.d"
+  "CMakeFiles/main.dir/home/langningc2009/Coding/Lib/StringOperation.cpp.o"
+  "CMakeFiles/main.dir/home/langningc2009/Coding/Lib/StringOperation.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
