3e31f47f4e2d3f1878ad256aa873d7aa9e0aa78d
langningc2009 <jpxx_chenlangning@outlook.com>
2023-07-12 20:38:06 +0800

    sync

--- Run.sh
+++ Run.sh
@@ -3,4 +3,4 @@ cmake -B build
 cmake --build build
 set Username (cat Username)
 set Password (cat Password)
-./build/main -u $Username -p $Password
+./build/main -u $Username -p $Password -g 9 -s 0 -sj 英语
